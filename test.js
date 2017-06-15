996ac0c1f9d4beebe5b2c2e68c59bd342a5327bd
Daniel Lauzon <daniel.lauzon@irdeto.com>
2017-06-15 19:51:49 -0400

    remove es6 (=>) syntax

--- test.js
+++ test.js
@@ -56,10 +56,10 @@ tape('regular input', function (t) {
 })
 
 const input = 'I am just a little bit of content'
-tape(`zlib gzip|gunzip a buffer`, (t) => {
-  zlib.gzip(input, (err, buffer1) => {
+tape(`zlib gzip|gunzip a buffer`, function (t) {
+  zlib.gzip(input, function (err, buffer1) {
     if (err) { return t.end(err) }
-    zlib.gunzip(buffer1, (err, buffer2) => {
+    zlib.gunzip(buffer1, function (err, buffer2) {
       if (err) { return t.end(err) }
       t.same(buffer2.toString(), input, 'gzip|gunzip === identity')
       t.end()
@@ -67,12 +67,12 @@ tape(`zlib gzip|gunzip a buffer`, (t) => {
   })
 })
 
-tape('zlib.gunzip a truncated buffer', (t) => {
-  zlib.gzip(input, (err, gzBuf) => {
+tape('zlib.gunzip a truncated buffer', function (t) {
+  zlib.gzip(input, function (err, gzBuf) {
     if (err) { return t.end(err) }
 
     const gzBufTruncated = gzBuf.slice(0, gzBuf.length / 2)
-    zlib.gunzip(gzBufTruncated, (err, buffer) => {
+    zlib.gunzip(gzBufTruncated, function (err, buffer) {
       if (err) {
         t.same(err.message, 'unexpected end of file', 'expected error')
         return t.end()
@@ -84,22 +84,22 @@ tape('zlib.gunzip a truncated buffer', (t) => {
 })
 
 // Does not work on node:8.1
-tape(`Handle error in truncated gunzip-maybe stream`, (t) => {
+tape(`Handle error in truncated gunzip-maybe stream`, function (t) {
   const stream = require('stream')
 
   const gzip = gunzip()
-  gzip.on('error', err => {
+  gzip.on('error', function (err) {
     t.same(err.message, 'unexpected end of file', 'expected error')
     t.end()
   })
-  gzip.on('end', () => {
+  gzip.on('end', function () {
     t.end(new Error('Should not reach the end...'))
   })
 
   const readStream = new stream.PassThrough()
   readStream.pipe(gzip)
 
-  zlib.gzip(input, (err, gzBuf) => {
+  zlib.gzip(input, function (err, gzBuf) {
     if (err) { return t.end(err) }
 
     const gzBufTruncated = gzBuf.slice(0, gzBuf.length / 2)
