1d7193bac8c4f7f703bd9d22f1a4a6530570740e
Daniel Lauzon <daniel.lauzon@irdeto.com>
2017-06-15 19:44:05 -0400

    add a test which fails on node:8

--- test.js
+++ test.js
@@ -54,3 +54,55 @@ tape('regular input', function (t) {
       t.end()
     }))
 })
+
+const input = 'I am just a little bit of content'
+tape(`zlib gzip|gunzip a buffer`, (t) => {
+  zlib.gzip(input, (err, buffer1) => {
+    if (err) { return t.end(err) }
+    zlib.gunzip(buffer1, (err, buffer2) => {
+      if (err) { return t.end(err) }
+      t.same(buffer2.toString(), input, 'gzip|gunzip === identity')
+      t.end()
+    })
+  })
+})
+
+tape('zlib.gunzip a truncated buffer', (t) => {
+  zlib.gzip(input, (err, gzBuf) => {
+    if (err) { return t.end(err) }
+
+    const gzBufTruncated = gzBuf.slice(0, gzBuf.length / 2)
+    zlib.gunzip(gzBufTruncated, (err, buffer) => {
+      if (err) {
+        t.same(err.message, 'unexpected end of file', 'expected error')
+        return t.end()
+      }
+      // expect(buffer.toString()).to.equal(input)
+      t.end(new Error('Should not happen'))
+    })
+  })
+})
+
+// Does not work on node:8.1
+tape(`Handle error in truncated gunzip-maybe stream`, (t) => {
+  const stream = require('stream')
+
+  const gzip = gunzip()
+  gzip.on('error', err => {
+    t.same(err.message, 'unexpected end of file', 'expected error')
+    t.end()
+  })
+  gzip.on('end', () => {
+    t.end(new Error('Should not reach the end...'))
+  })
+
+  const readStream = new stream.PassThrough()
+  readStream.pipe(gzip)
+
+  zlib.gzip(input, (err, gzBuf) => {
+    if (err) { return t.end(err) }
+
+    const gzBufTruncated = gzBuf.slice(0, gzBuf.length / 2)
+    readStream.end(gzBufTruncated)
+  })
+})
